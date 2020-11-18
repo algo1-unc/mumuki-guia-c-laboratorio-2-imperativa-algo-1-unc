7920f77d6743d96f2ddfdd3b6a71da95a7f2ae4b
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-11-18 05:37:05 +0000

    Mumuki Export on 2020-11-18 05:37:05 +0000

--- 00005_Struct_ División/test.c
+++ 00005_Struct_ División/test.c
@@ -16,7 +16,60 @@ describe ("Test: ") {
     it (genMsj(dividendo, divisor, st_expected.cociente, st_expected.resto, st_expected.excepcion)) {
         should_int(st_student.cociente) be equal to(st_expected.cociente);
         should_int(st_student.resto) be equal to(st_expected.resto);
-        should_bool(st_student.exception) be equal to(st_expected.exception);
+        should_bool(st_student.excepcion) be equal to(st_expected.excepcion);
     } end
+
+    // ---
+    dividendo = 0;
+    divisor = 3;
+    
+    st_student = division(dividendo, divisor);
+    st_expected = DIVISION(dividendo, divisor);
+
+    it (genMsj(dividendo, divisor, st_expected.cociente, st_expected.resto, st_expected.excepcion)) {
+        should_int(st_student.cociente) be equal to(st_expected.cociente);
+        should_int(st_student.resto) be equal to(st_expected.resto);
+        should_bool(st_student.excepcion) be equal to(st_expected.excepcion);
+    } end
+
+    // ---
+    dividendo = 12;
+    divisor = 5;
+    
+    st_student = division(dividendo, divisor);
+    st_expected = DIVISION(dividendo, divisor);
+
+    it (genMsj(dividendo, divisor, st_expected.cociente, st_expected.resto, st_expected.excepcion)) {
+        should_int(st_student.cociente) be equal to(st_expected.cociente);
+        should_int(st_student.resto) be equal to(st_expected.resto);
+        should_bool(st_student.excepcion) be equal to(st_expected.excepcion);
+    } end
+
+    // ---
+    dividendo = 4;
+    divisor = 0;
+    
+    st_student = division(dividendo, divisor);
+    st_expected = DIVISION(dividendo, divisor);
+
+    it (genMsj(dividendo, divisor, st_expected.cociente, st_expected.resto, st_expected.excepcion)) {
+        should_int(st_student.cociente) be equal to(st_expected.cociente);
+        should_int(st_student.resto) be equal to(st_expected.resto);
+        should_bool(st_student.excepcion) be equal to(st_expected.excepcion);
+    } end
+
+    // ---
+    dividendo = 0;
+    divisor = 0;
+    
+    st_student = division(dividendo, divisor);
+    st_expected = DIVISION(dividendo, divisor);
+
+    it (genMsj(dividendo, divisor, st_expected.cociente, st_expected.resto, st_expected.excepcion)) {
+        should_int(st_student.cociente) be equal to(st_expected.cociente);
+        should_int(st_student.resto) be equal to(st_expected.resto);
+        should_bool(st_student.excepcion) be equal to(st_expected.excepcion);
+    } end
+
     
 } end
