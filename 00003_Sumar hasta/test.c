bb40d488130ff207a9c5b62383f19b0333b235f8
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-11-18 03:38:43 +0000

    Mumuki Export on 2020-11-18 03:38:43 +0000

--- 00003_Sumar hasta/test.c
+++ 00003_Sumar hasta/test.c
@@ -4,19 +4,19 @@ int n, expected;
 
 describe ("Test: ") {
     
-    n = 0
+    n = 0;
     expected = SUMAR_HASTA(n);
     it (genMsj(n,expected)) {
         should_int(sumarHasta(n)) be equal to(expected);
     } end
     
-    n = 4
+    n = 4;
     expected = SUMAR_HASTA(n);
     it (genMsj(n,expected)) {
         should_int(sumarHasta(n)) be equal to(expected);
     } end
 
-    n = 11
+    n = 11;
     expected = SUMAR_HASTA(n);
     it (genMsj(n,expected)) {
         should_int(sumarHasta(n)) be equal to(expected);
