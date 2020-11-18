d90e9bdc42c90f4f9b1ab280df305da0b4b46b70
hernan.maina@mi.unc.edu.ar <hernan.maina@mi.unc.edu.ar>
2020-11-18 02:17:14 +0000

    Mumuki Export on 2020-11-18 02:17:14 +0000

--- 00001_Funciones en C, Assert/test.c
+++ 00001_Funciones en C, Assert/test.c
@@ -2,34 +2,30 @@
 
 int x,y,expected;
 
-describe ("Mínimo") {
-  
-  describe ("Igual signo") {
+describe ("Distancia") {
     
     x, y = 15,2;
     expected = DISTANCIA(x,y);
-    it (genMsg(x,y,expected)) {
+    it (genMsj(x,y,expected)) {
         should_int(distancia(x,y)) be equal to(expected);
     } end
 
     x, y = -15,-2;
     expected = DISTANCIA(x,y);
-    it (genMsg(x,y,expected)) {
+    it (genMsj(x,y,expected)) {
         should_int(distancia(x,y)) be equal to(expected);
     } end
 
     x, y = 0,0;
     expected = DISTANCIA(x,y);
-    it (genMsg(x,y,expected)) {
+    it (genMsj(x,y,expected)) {
         should_int(distancia(x,y)) be equal to(expected);
     } end
 
     x, y = -50,100;
     expected = DISTANCIA(x,y);
-    it (genMsg(x,y,expected)) {
+    it (genMsj(x,y,expected)) {
         should_int(distancia(x,y)) be equal to(expected);
     } end
-
-  } end
   
 } end
